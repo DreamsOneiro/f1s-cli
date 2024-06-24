94d0604b08ba6a0a0c980677aabca1cef05a1c29
DreamsOneiro <dreams.kong@gmail.com>
2024-06-25 02:40:11 +0800

    Auto use local timezone

--- src/time.rs
+++ src/time.rs
@@ -1,7 +1,7 @@
-use chrono::{prelude::{DateTime, Utc}, FixedOffset};
+use chrono::{prelude::{DateTime, Utc}, FixedOffset, Local};
 
 pub fn gmt8_str(time: &DateTime<Utc>) -> String {
-    time.with_timezone(&FixedOffset::east_opt(8 * 3600).unwrap())
+    time.with_timezone(&FixedOffset::east_opt(get_timezone()).unwrap())
         .format("%b %e, %a | %I:%M%p")
         .to_string()
 }
@@ -10,4 +10,9 @@ pub fn to_utc(race_date: &str, race_time: &str) -> DateTime<Utc> {
     let time: String = format!("{}T{}", race_date, race_time);
     let time = time.parse::<DateTime<Utc>>().expect("Problem converting time");
     time
+}
+
+fn get_timezone() -> i32{
+    let offset_in_sec: i32 = Local::now().offset().local_minus_utc();
+    offset_in_sec
 }
